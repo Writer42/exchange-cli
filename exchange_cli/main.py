d8ea311aca2ad4fa9b2d187a4c45be478929aef7
Writer42 <writer42@mail.ru>
2023-01-27 12:23:30 +0600

    remove some newlines

--- exchange_cli/main.py
+++ exchange_cli/main.py
@@ -22,26 +22,26 @@ def parse():
         "history", help="show conversion rate for range of dates")
 
     convert.add_argument("--from", dest="from_symbols",
-                        required=True, help="from currency code")
+                         required=True, help="from currency code")
     convert.add_argument("--to", dest="to_symbols",
-                        required=True, help="to currency code")
-    convert.add_argument("amount", type=int, help="amount of currency to exchange")
+                         required=True, help="to currency code")
+    convert.add_argument("amount", type=int,
+                         help="amount of currency to exchange")
 
     history.add_argument("--from", dest="from_symbols",
-                        required=True, help="from currency code")
+                         required=True, help="from currency code")
     history.add_argument("--date_from", required=True)
     history.add_argument("--date_to", required=True)
     history.add_argument("--to", dest="to_symbols",
-                        required=True, help="to currency code")
-    history.add_argument("amount", type=int, help="amount of currency to exchange")
+                         required=True, help="to currency code")
+    history.add_argument("amount", type=int,
+                         help="amount of currency to exchange")
 
     args = parser.parse_args()
 
     if args.command == "symbols":
         symbols_list = asyncio.run(get_currency_list())
         print_text_in_columns(symbols_list)
-        
-
     elif args.command == "convert":
         asyncio.run(print_exchange_rate(
             args.from_symbols, args.to_symbols, args.amount))
@@ -50,4 +50,3 @@ def parse():
                     args.date_to, args.from_symbols, args.to_symbols, args.amount))
     else:
         parser.print_help()
-
